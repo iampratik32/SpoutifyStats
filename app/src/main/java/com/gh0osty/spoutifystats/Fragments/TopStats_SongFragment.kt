0dd024f5b6f4219fab95809566ee68025a32c85e
Pratik Poudel <iampratik32@gmail.com>
2022-01-03 20:39:22 +0545

    Top Songs Card Added

--- app/src/main/java/com/gh0osty/spoutifystats/Fragments/TopStats_SongFragment.kt
+++ app/src/main/java/com/gh0osty/spoutifystats/Fragments/TopStats_SongFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gh0osty.spoutifystats.Adapters.TopArtistAdapter
+import com.gh0osty.spoutifystats.Adapters.TopSongAdapter
 import com.gh0osty.spoutifystats.R
+import com.gh0osty.spoutifystats.ViewModels.TopArtistViewModel
+import com.gh0osty.spoutifystats.ViewModels.TopSongViewModel
 
 class TopStats_SongFragment : Fragment() {
 
@@ -16,7 +22,14 @@ class TopStats_SongFragment : Fragment() {
 
         val view: View = inflater.inflate(R.layout.fragment_top_stats__song, container, false)
 
-
+        val recyclerView: RecyclerView = view.findViewById(R.id.topStatsSong_recyclerView)
+        recyclerView.layoutManager = GridLayoutManager(activity,2)
+        val data = ArrayList<TopSongViewModel>()
+        for (i in 0..50) {
+            data.add(TopSongViewModel("S${i}","Artist ${i+1}","Song ${i+1}",R.drawable.spotify))
+        }
+        val adapter = TopSongAdapter(data,context)
+        recyclerView.adapter = adapter
 
         return view
 
---
+++ app/src/main/java/com/gh0osty/spoutifystats/ViewModels/TopSongViewModel.kt
@@ -0,0 +1,4 @@
+package com.gh0osty.spoutifystats.ViewModels
+
+class TopSongViewModel(val id: String, val artist: String, val text: String, val image: Int) {
+}
